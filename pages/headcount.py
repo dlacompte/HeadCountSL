0cc28efcae718ad50944cd019d7bce1b8a581016
sherryzha <sherryruiting.zha@outlook.com>
2022-11-29 02:16:55 -0600

    Add files via upload

--- pages/headcount.py
+++ pages/headcount.py
@@ -38,7 +38,7 @@ class MyProgram:
 
         if shrinkage < 0 or shrinkage >= 1:
             raise ValueError("shrinkage must be between in the interval [0,1)")
-            
+
         if occupancy < 0 or occupancy > 1:
             raise ValueError("occupancy must be between in the interval [0,1)")
 
@@ -63,7 +63,7 @@ class MyProgram:
 
         erlang_b = 1 / erlang_b_inverse
         return productive_positions * erlang_b / (productive_positions - self.intensity * (1 - erlang_b))
-        
+
     def service_level(self, positions: int, scale_positions: bool = True):
         if scale_positions:
             productive_positions = floor((1 - self.shrinkage) * positions )
@@ -73,8 +73,8 @@ class MyProgram:
         probability_wait = self.waiting_probability(productive_positions, scale_positions=False)
         exponential = exp(-(productive_positions - self.intensity) * (self.asa / self.aht))
         return max(0, 1 - (probability_wait * exponential))
-    
- 
+
+
     def achieved_occupancy(self, positions: int, scale_positions: bool = False):
         if scale_positions:
             productive_positions = floor((1 - self.shrinkage) * positions)
@@ -82,13 +82,13 @@ class MyProgram:
             productive_positions = positions
 
         return self.intensity / productive_positions
-    
+
     def required_positions(self, service_level: float):
         if service_level < 0 or service_level > 1:
             raise ValueError("service_level must be between 0 and 1")
 
         positions = round(self.intensity + 1)
-        achieved_service_level = self.service_level(positions, scale_positions=False) 
+        achieved_service_level = self.service_level(positions, scale_positions=False)
         while achieved_service_level < service_level:
             positions += 1
             achieved_service_level = self.service_level(positions, scale_positions=False)
@@ -125,10 +125,10 @@ def load_com_hc():
     return model_hc
 
 #load RMSE
-RMSE_com_hc = 2.9857452688920056
-RMSE_res_hc = 15.773700582036296
-RMSE_myq_hc = 14.272518249073393
-columns = ['SL', 'Call Volume','AHT','ASA','Occupany %','Shrinkage %']
+RMSE_com_hc = 2.5656574352948165
+RMSE_res_hc = 14.121853781129152
+RMSE_myq_hc = 14.727407096077105
+columns = ['SL', 'Call Volume','AHT','ASA','Occupancy %','Shrinkage %']
 
 # page config
 st.set_page_config(page_title="Headcount Calculator", page_icon="👩‍💻")
@@ -140,11 +140,8 @@ def res_hc_prediction(param,CI):
     upper = alpha/2
     Z = stat.norm.ppf(1-upper)          #Above three line for Calculating Z-score
     param = pd.DataFrame([param], columns=columns)
-    #print(param)
     model_hc = load_res_hc()
     result = model_hc.predict(param)[0]    #Prediction of HC
-    #print(result)
-    #result = float(str(result)[1:-1])      #Removing bracket from array results
     low = result - Z* RMSE_res_hc                #Upper and Lower bound
     high = result + Z * RMSE_res_hc
     return result, low, high
@@ -154,11 +151,8 @@ def com_hc_prediction(param,CI):
     upper = alpha/2
     Z = stat.norm.ppf(1-upper)          #Above three line for Calculating Z-score
     param = pd.DataFrame([param], columns=columns)
-    #print(param)
     model_hc = load_com_hc()
     result = model_hc.predict(param)[0]    #Prediction of HC
-    #print(result)
-    #result = float(str(result)[1:-1])      #Removing bracket from array results
     low = result - Z* RMSE_com_hc                #Upper and Lower bound
     high = result + Z * RMSE_com_hc
     return result, low, high
@@ -168,11 +162,8 @@ def myq_hc_prediction(param,CI):
     upper = alpha/2
     Z = stat.norm.ppf(1-upper)          #Above three line for Calculating Z-score
     param = pd.DataFrame([param], columns=columns)
-    #print(param)
     model_hc = load_myq_hc()
     result = model_hc.predict(param)[0]    #Prediction of HC
-    #print(result)
-    #result = float(str(result)[1:-1])      #Removing bracket from array results
     low = result - Z* RMSE_myq_hc                #Upper and Lower bound
     high = result + Z * RMSE_myq_hc
     return result, low, high
@@ -205,7 +196,7 @@ with header:
 with lob_and_freq:
     st.subheader("Select Line of Business and Frequency")
     lob_col, freq_col, days_col = st.columns(3)
-    line_of_business_text = lob_col.selectbox('Line of Business', options = ['COMM', 'RESI', 'MYQ'], index = 0)
+    line_of_business_text = lob_col.selectbox('Line of Business', options = ['COMM', 'RESI', 'MYQ'], index = 1)
     if line_of_business_text == 'COMM':
         lob = 0
     elif line_of_business_text == 'RESI':
@@ -214,8 +205,8 @@ with lob_and_freq:
         lob = 2
     else:
         lob = 0
-    
-    freq_text = freq_col.selectbox('Frequency', options=['Weekly', 'Daily'], index=1)
+
+    freq_text = freq_col.selectbox('Frequency', options=['Weekly', 'Daily'], index=0)
     if freq_text=='Weekly':
         if lob==0:
             num_of_days = days_col.number_input('Number of Workdays in the Week', min_value=1, max_value=7, value=5, step=1)
@@ -223,42 +214,45 @@ with lob_and_freq:
             num_of_days = days_col.number_input('Number of Workdays in the Week', min_value=1, max_value=7, value=6, step=1)
 
 with parameter:
-    
+
     st.subheader('Enter Parameters')
     left_col, mid_col, right_col = st.columns(3)
-    
+
     #catch necesary parameters
-    service_level = left_col.number_input(label='Service Level Target %', 
+    service_level = left_col.number_input(label='Service Level Target %',
                                         min_value=0.0,
                                         max_value=100.0,
-                                        value = 80.0,
+                                        value = 90.0,
                                         step=1.0)/100
-       
-    
-    
-    total_call_volume = left_col.number_input(label='Total Calls Volumes',
-                                        min_value=0,
-                                        value=2000)
 
-    
+
+
+    total_call_volume = left_col.number_input(label='Total Call Volumes',
+                                        min_value=0,
+                                        value=14800)
+
+
+    aht = mid_col.number_input('Average Handling Time',min_value=0, value=900,
+                                help='Enter your estimated AHT in seconds')
+
+    apa = mid_col.number_input('Average Speed of Answer', min_value = 0, value=50,
+                                help='Enter your estimated ASA in seconds, NOT the acceptable wait time')
+
+    occupancy = right_col.number_input('Maximum Occupancy %', min_value=0.0, max_value=100.0, value=65.0, step=1.0)/100
+
+    shrinkage = right_col.number_input('Shrinkage %', min_value=0.0, max_value=100.0, value=40.0, step=1.0)/100
+
     # Now I take average for weekly call volume, will be upgraded later
     if freq_text == 'Weekly':
         call_volume = total_call_volume/num_of_days
         confidence_level = 0.95  #won't be used
-        
+
     else:
         call_volume = total_call_volume
-        confidence_level = st.slider('Confidence Level %', min_value = 80, max_value = 99, value = 90, step = 1)/100
+        confidence_level = left_col.selectbox('Confidence Level %', options=[90, 95, 99], index=0)/100
+        opening_hours = mid_col.number_input('Length of Opening Hours', min_value = 1.0, max_value=24.0, value=13.0, step=1.0,
+                                    help='Hours between the opening and closing of the call center; only used in Erlang C results')
 
-    
-    aht = mid_col.number_input('Average Handling Time in Seconds',min_value=0, value=700)
-    
-    apa = mid_col.number_input('Average Speed of Answer in Seconds', min_value = 0, value=100)
-
-    occupancy = right_col.number_input('Maximum Occupancy %', min_value=0.0, max_value=100.0, value=80.0, step=1.0)/100
-    
-    shrinkage = right_col.number_input('Shrinkage %', min_value=0.0, max_value=100.0, value=30.0, step=1.0)/100
-      
     param = np.array([service_level, call_volume, aht, apa, occupancy, shrinkage])
 
     # display the results
@@ -282,8 +276,8 @@ with model_prediction:
             param_max = np.array([service_level, call_volume*1.326, aht, apa, occupancy, shrinkage])
             avg_hc = max(get_HC(param, confidence_level, res_hc_prediction),0)
             min_hc = max(get_HC(param_min, confidence_level, res_hc_prediction),0)
-            max_hc = get_HC(param_max, confidence_level, res_hc_prediction)            
-            left.metric(label=hc_label, value=round(avg_hc, 1)) 
+            max_hc = get_HC(param_max, confidence_level, res_hc_prediction)
+            left.metric(label=hc_label, value=round(avg_hc, 1))
             mid.metric(label='Min Daily Headcount in the Week', value=round(min(min_hc, avg_hc), 1))
             right.metric(label='Max Daily Headcount in the Week', value=round(max(max_hc, avg_hc), 1))
         else:
@@ -292,7 +286,7 @@ with model_prediction:
             avg_hc = max(get_HC(param, confidence_level, myq_hc_prediction),0)
             min_hc = max(get_HC(param_min, confidence_level, myq_hc_prediction),0)
             max_hc = get_HC(param_max, confidence_level, myq_hc_prediction)
-            left.metric(label=hc_label, value=round(avg_hc, 1)) 
+            left.metric(label=hc_label, value=round(avg_hc, 1))
             mid.metric(label='Min Daily Headcount in the Week', value=round(min(min_hc, avg_hc), 1))
             right.metric(label='Max Daily Headcount in the Week', value=round(max(max_hc, avg_hc), 1))
     else:
@@ -307,27 +301,20 @@ with model_prediction:
             right.metric(label='Upper Bound of Confidence Interval', value=round(get_HC_high(param, confidence_level, res_hc_prediction), 1))
         else:
             left.metric(label=hc_label, value=round(get_HC(param, confidence_level, myq_hc_prediction), 1))
-            mid.metric(label='Lower Bound of Confidence Interval', value=round(get_HC_low(param, confidence_level, myq_hc_prediction), 1)) 
+            mid.metric(label='Lower Bound of Confidence Interval', value=round(get_HC_low(param, confidence_level, myq_hc_prediction), 1))
             right.metric(label='Upper Bound of Confidence Interval', value=round(get_HC_high(param, confidence_level, myq_hc_prediction), 1))
-    
+
 
 with erlangC_prediction:
     st.subheader('Erlang C Results')
     left, mid, right = st.columns(3)
     if freq_text == 'Weekly':
-        ec = MyProgram(transactions=total_call_volume, 
-                    asa=apa/60, aht=aht/60, interval=60*8*num_of_days, shrinkage=shrinkage, occupancy=occupancy)
+        ec = MyProgram(transactions=total_call_volume,
+                    asa=apa/60, aht=aht/60, interval=60*13*num_of_days, shrinkage=shrinkage, occupancy=occupancy)
     else:
-        ec = MyProgram(transactions=total_call_volume, 
-                    asa=apa/60, aht=aht/60, interval=60*8, shrinkage=shrinkage, occupancy=occupancy)       
+        ec = MyProgram(transactions=total_call_volume,
+                    asa=apa/60, aht=aht/60, interval=60*opening_hours, shrinkage=shrinkage, occupancy=occupancy)
     left.metric(label='HeadCount', value=ec.required_positions(service_level=service_level)['positions'])
 
     #mid.write(str(round(ec.required_positions(service_level=service_level)['service_level']*100,2))+'%')
     mid.metric(label='Service Level', value=str(round(ec.required_positions(service_level=service_level)['service_level']*100,2))+'%')
-    
-
-
-    
-
-
-
